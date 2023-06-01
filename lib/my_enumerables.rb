c9d248649670607bcf763e0acd4b63398954e11a
Jorge <13945741+jorgeblanez@users.noreply.github.com>
2023-06-01 00:00:20 -0300

    First Commit

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,64 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    for i in 0..self.length-1 do
+      yield self[i], i
+    end
+    self
+  end
+
+  def my_select
+    array=[]
+    for element in self do
+      if yield element then array << element end
+    end
+    array
+  end
+
+  def my_all?
+    for element in self do
+      unless yield element then return false end
+    end
+    true
+  end
+
+  def my_any?
+    for element in self do
+      if yield element then return true end
+    end
+    false
+  end
+
+  def my_none?
+    for element in self do
+      if yield element then return false end
+    end
+    true
+  end
+
+  def my_count
+    return self.size unless block_given?
+    count = 0
+    for element in self do
+      if yield element then count+=1 end
+    end
+    count
+  end
+
+  def my_map
+    enum = []
+    for element in self do
+      enum << (yield element)
+    end
+    enum
+  end
+
+  def my_inject (value)
+    for element in self do
+      value = (yield value,element)
+    end
+    value
+  end
+  
 end
 
 # You will first have to define my_each
@@ -8,4 +67,11 @@ end
 # to this method
 class Array
   # Define my_each here
+
+  def my_each
+    for element in self do
+      yield element
+    end
+    self
+  end
 end
